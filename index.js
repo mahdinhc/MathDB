5b33b621aa9e757b58dac6f9d73557257c446360
2DPrototype <miaccess077dev@gmail.com>
2025-03-08 15:51:19 +0600

    modified

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const owner = "mahdinhc";
 const repo = "MathDB";      
 const branch = "main";   
 const directory = "database"; 
-const apiUrl = `https://api.github.com/repos/${owner}/${repo}/git/trees/${branch}?recursive=1`;
+const apiUrl = `https://api.github.com/repos/${owner}/${repo}/git/trees/${branch}?recursive=1`
 
 function getBasename(path) {
-  const base = path.split(/[\\/]/).pop();
-  const dotIndex = base.lastIndexOf('.');
-  return dotIndex > 0 ? base.substring(0, dotIndex) : base;
+	const base = path.split(/[\\/]/).pop()
+	const dotIndex = base.lastIndexOf('.')
+	return dotIndex > 0 ? base.substring(0, dotIndex) : base
 }
 
 function parseMath(str) {
@@ -21,32 +21,40 @@ function parseMath(str) {
 }
 
 function buildJsTreeData(flatTree, directory) {
-  const treeData = [];
-  const dirPrefix = directory ? `${directory}/` : "";
+	const treeData = [];
+	const dirPrefix = directory ? `${directory}/` : ""
 
-  flatTree.forEach(item => {
-    if (!item.path.startsWith(dirPrefix)) return;
+	flatTree.forEach(item => {
+		if (!item.path.startsWith(dirPrefix)) return
 
-    let relativePath = item.path.substring(dirPrefix.length); 
-    if (!relativePath) return; 
+		let relativePath = item.path.substring(dirPrefix.length); 
+		if (!relativePath) return
 
-    let parent = "#";
-    if (relativePath.includes('/')) {
-      parent = relativePath.substring(0, relativePath.lastIndexOf('/'));
-      if (parent === "") {
-        parent = "#";
-      }
-    }
+		let parent = "#";
+		if (relativePath.includes('/')) {
+			parent = relativePath.substring(0, relativePath.lastIndexOf('/'))
+			if (parent === "") {
+				parent = "#"
+			}
+		}
+		
+		const ext = item.path.substring(item.path.lastIndexOf('.'))
+		let text = relativePath.split('/').pop()
+		if (ext == ".md") text = `<i>${parseMath(text)}</i>`
 
-    treeData.push({
-      id: relativePath,
-      parent: parent, 
-      text: relativePath.split('/').pop(), 
-      icon: item.type === 'tree' ? "jstree-folder" : "jstree-file"
-    });
-  });
+		treeData.push({
+			id: relativePath,
+			parent: parent, 
+			text: text, 
+			icon: item.type === 'tree' ? "icons/folder.svg" : "icons/file.svg",
+			data: {
+				ext: ext,
+				type: item.type === 'tree' ? "folder" : "file",
+			}
+		})
+	})
 
-  return treeData;
+	return treeData
 }
 
 function updateNode() {
@@ -72,12 +80,11 @@ $(document).ready(function() {
 			.click('redraw.jstree', updateNode)
 			.on('select_node.jstree', function(e, data) {
 				const node = data.node;
-				if (node.icon === "jstree-file") {
-					console.log(node.id)
+				if (node.data.type == "file") {
 					const filePath = directory ? `${directory}/${node.id}` : node.id
 					const rawUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${filePath}`
 					$.get(rawUrl, function(fileData) {
-						if (node.id.substring(node.id.lastIndexOf('.')) == ".asc") {
+						if (node.data.ext == ".asc") {
 							$('#fileContent').html(parseMath(fileData))
 						}
 						else $('#fileContent').html(fileData)
@@ -94,4 +101,28 @@ $(document).ready(function() {
 	.fail(function(jqXHR, textStatus, errorThrown) {
 		$('#repoTree').html('<p>Error fetching repository tree data. Please check the repository details and try again.</p>')
 	})
+})
+
+
+function renderAsciiMath() {
+	const input = document.getElementById("input").value
+	document.getElementById("output").innerHTML = ""
+	document.getElementById("output").append(parseMath(input))
+}
+
+document.getElementById("input").addEventListener("input", renderAsciiMath)
+window.onload = renderAsciiMath
+
+$("#mode-renderer").click(function(){
+	$(this).hide()
+	$("#container-1").hide()
+	$("#mode-database").show()
+	$("#container-2").show()
+})
+
+$("#mode-database").click(function(){
+	$(this).hide()
+	$("#container-2").hide()
+	$("#mode-renderer").show()
+	$("#container-1").show()
 })
